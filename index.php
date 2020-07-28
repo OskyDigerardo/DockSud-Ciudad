9d6c29229f57b884089e5888cbacf2aa600d88d0
OskyDigerardo <digerardo.oscar@gmail.com>
2020-07-28 02:35:20 -0300

    primer-video

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $url = !empty($_GET['url']) ? $_GET['url'] : 'home/home';
+    $arrUrl = explode("/", $url);
+    $controller = $arrUrl[0];
+    $method = $arrUrl[0];
+    $params = "";
+
+    if(!empty($arrUrl[1])){
+        if($arrUrl[1] != ""){
+            $method = $arrUrl[1];
+        }
+    }
+
+    echo "controlado: ".$controller;
+    echo "<br>";
+    echo "método: ".$method;
+
+?>
